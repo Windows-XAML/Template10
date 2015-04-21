4b99d6b78fba8c6bbb91d9a64a5caba25e48417c
JerryNixon <jerry@online.jerrynixon.com>
2015-04-21 09:09:20 -0700

    Updated Converters

---
+++ Templates (Project)/TODO/TODO/Converters/StateToStringConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Template10.Converters
+{
+    class StateToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var state = (Models.States)value;
+            switch (state)
+            {
+                case Models.States.NotStarted:
+                    return "Not Started";
+                case Models.States.InProcess:
+                    return "In Process";
+                default:
+                    return "Done";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
