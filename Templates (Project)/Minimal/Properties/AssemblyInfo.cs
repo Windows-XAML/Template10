e6b8757eb501c5f911f16fff2f1173a655fb0b3b
Daren May <daren.may@gmail.com>
2015-07-29 14:57:25 -0600

    Migrating Project Apps and Template10Library to RTM UWP tools

--- Templates (Project)/Minimal/Properties/AssemblyInfo.cs
+++ Templates (Project)/Minimal/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Blank1")]
-[assembly: AssemblyDescription("Template 10")]
+[assembly: AssemblyTitle("Minimal")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Blank1")]
+[assembly: AssemblyProduct("Minimal")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
