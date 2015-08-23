cb6c8af33f4b83b1c3a8abb8cde26d7f48dfb1b0
Johannes Ellinger <kopfkraft@hotmail.com>
2015-08-23 08:43:27 +0200

    removed old settings classes, jsonpropertymapper per default

--- Template10Library/Utils/PropertyMapping.cs
+++ Template10Library/Utils/PropertyMapping.cs
@@ -1,10 +1,5 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Template10.Utils
 {
@@ -34,58 +29,4 @@ namespace Template10.Utils
 			return jsonConverter;
 		}
 	}
-
-	public class PropertyMapping : Dictionary<Type, IStoreConverter>, IPropertyMapping
-	{
-		protected IStoreConverter Fallback { get; }
-
-		public PropertyMapping()
-		{
-
-			this[typeof(int)] = new IntConverter();
-			this[typeof(string)] = new StringConverter();
-			this[typeof(DateTime)] = new DateTimeConverter();
-			this[typeof(DateTimeOffset)] = new DateTimeOffsetConverter();
-			this[typeof(TimeSpan)] = new TimeSpanConverter();
-
-			Fallback = new JsonConverter();
-		}
-
-		public IStoreConverter GetConverter(Type type)
-		{
-			return ContainsKey(type) ? this[type] : Fallback;
-		}
-	}
-
-	public class IntConverter : IStoreConverter
-	{
-		public object FromStore(string value, Type type) => int.Parse(value);
-		public string ToStore(object value, Type type) => value.ToString();
-	}
-
-	public class StringConverter : IStoreConverter
-	{
-		public object FromStore(string value, Type type) => value;
-		public string ToStore(object value, Type type) => value.ToString();
-	}
-
-	public class DateTimeConverter : IStoreConverter
-	{
-		public object FromStore(string value, Type type) => new DateTime(long.Parse(value));
-		public string ToStore(object value, Type type) => ((DateTime)value).Ticks.ToString();
-	}
-
-	public class DateTimeOffsetConverter : IStoreConverter
-	{
-		protected DateTimeConverter ProxyConverter = new DateTimeConverter();
-		public object FromStore(string value, Type type) => new DateTimeOffset((DateTime)ProxyConverter.FromStore(value, type));
-		public string ToStore(object value, Type type) => ((DateTimeOffset)value).Ticks.ToString();
-	}
-
-	public class TimeSpanConverter : IStoreConverter
-	{
-		public object FromStore(string value, Type type) => TimeSpan.FromTicks(long.Parse(value));
-		public string ToStore(object value, Type type) => ((TimeSpan)value).Ticks.ToString();
-	}
-
 }
