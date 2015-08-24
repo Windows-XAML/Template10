eec6b3d96e7e587517fb5664b715cf98acecbebf
Jerry Nixon <jerry@online.jerrynixon.com>
2015-08-24 14:03:52 -0600

    Prepping for the future: INPC with IBindable.

---
+++ Template10Library/Mvvm/IBindable.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Template10.Mvvm
+{
+    // this exists for the future implementation of the INPC property attribute
+    public interface IBindable : INotifyPropertyChanged
+    {
+        void RaisePropertyChanged([CallerMemberName]string propertyName = null);
+    }
+}
