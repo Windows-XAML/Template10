517dbc2012a21bee232c2f93eca179f0976acd46
Jerry Nixon <jerry@online.jerrynixon.com>
2015-08-25 11:17:24 -0600

    Added Add() to StateItems.

--- Template10Library/Common/StateItems.cs
+++ Template10Library/Common/StateItems.cs
@@ -8,7 +8,26 @@ namespace Template10.Common
 {
     public class StateItems : List<StateItem>
     {
-        public bool Contains(Type type, String key)
+        public StateItem Add(Type type, string key, object value)
+        {
+            if (Contains(type, key))
+                throw new ArgumentException("Same type+key exists.");
+            var item = new StateItem
+            {
+                Type = type,
+                Key = key,
+                Value = value
+            };
+            this.Add(item);
+            return item;
+        }
+
+        public bool Contains(Type type, string key, object value)
+        {
+            return this.Any(x => (x.Type?.Equals(type) ?? false) && (x.Key?.Equals(key) ?? false) && (x.Value == value));
+        }
+
+        public bool Contains(Type type, string key)
         {
             return this.Any(x => (x.Type?.Equals(type) ?? false) && (x.Key?.Equals(key) ?? false));
         }
@@ -18,12 +37,20 @@ namespace Template10.Common
             return this.Any(x => x.Value == value);
         }
 
-        public T Get<T>(String key)
+        public new bool Contains(StateItem item)
         {
-            return (T)this.First(x => x.Type == typeof(T) && x.Key == key)?.Value;
+            return Contains(item.Type, item.Key, item.Value);
         }
 
-        public bool TryGet<T>(String key, out T value)
+        public T Get<T>(string key)
+        {
+            var item = (T)this.First(x => x.Type == typeof(T) && x.Key == key)?.Value;
+            if (item == null)
+                throw new KeyNotFoundException();
+            return item;
+        }
+
+        public bool TryGet<T>(string key, out T value)
         {
             if (!Contains(typeof(T), key))
             {
