27c3475a9bedbd29ab0646010137fa1c13c64c0f
Johannes Ellinger <kopfkraft@hotmail.com>
2015-08-28 12:11:22 +0200

    settings according to #50 for remote

---
+++ Template10Library/Services/SettingsService/Settings.cs
@@ -0,0 +1,43 @@
+﻿using Windows.Foundation.Collections;
+using Windows.Storage;
+
+namespace Template10.Services.SettingsService
+{
+	// lazy initialization: https://github.com/Windows-XAML/Template10/pull/50
+	public class Settings : PropertyStore
+	{
+		private static Settings _local;
+		public static Settings Local
+		{
+			get
+			{
+				if (_local == null)
+					_local = new Settings(ApplicationData.Current.LocalSettings.Values);
+
+				return _local;
+			}
+		}
+
+		private static Settings _roaming;
+		public static Settings Roaming
+		{
+			get
+			{
+				if(_roaming == null)
+					_roaming = new Settings(ApplicationData.Current.RoamingSettings.Values);
+
+				return _roaming;
+			}
+		}
+
+		private static readonly IPropertyMapping Mapping;
+
+		static Settings()
+		{
+			Mapping = new JsonMapping();
+		}
+
+		private Settings(IPropertySet values) : base(values, Mapping)
+		{ }
+	}
+}
---
+++ Template10Library/Utils/TypeUtil.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Template10.Utils
+{
+	public static class TypeUtil
+	{
+		readonly static Type[] Primitives;
+
+		readonly static Type[] NullablePrimitives;
+
+		readonly static Type[] AllPrimitives;
+
+		static TypeUtil()
+		{
+			Primitives = new Type[]
+			{
+				typeof (Enum),
+				typeof (String),
+				typeof (Char),
+				typeof (Guid),
+
+				typeof (Boolean),
+				typeof (Byte),
+				typeof (Int16),
+				typeof (Int32),
+				typeof (Int64),
+				typeof (Single),
+				typeof (Double),
+				typeof (Decimal),
+
+				typeof (SByte),
+				typeof (UInt16),
+				typeof (UInt32),
+				typeof (UInt64),
+
+				typeof (DateTime),
+				typeof (DateTimeOffset),
+				typeof (TimeSpan),
+			};
+
+			NullablePrimitives = (from t in Primitives
+								  where t.GetTypeInfo().IsValueType
+								  select typeof(Nullable<>).MakeGenericType(t)).ToArray();
+
+			AllPrimitives = Primitives.Concat(NullablePrimitives).ToArray();
+		}
+
+		public static bool IsPrimitive(Type type)
+		{
+			if (AllPrimitives.Any(x => x.GetTypeInfo().IsAssignableFrom(type.GetTypeInfo())))
+				return true;
+
+			var nullable = Nullable.GetUnderlyingType(type);
+
+			return nullable != null && nullable.GetTypeInfo().IsEnum;
+		}
+	}
+}
