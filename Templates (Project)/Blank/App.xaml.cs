1814fc990a0217f9013a0cf32f2711538d241eb3
Callum Moffat <callum90ish@outlook.com>
2015-09-11 10:47:16 +1200

    Initial commit - added accent functionality to template App.xaml files.

--- Templates (Project)/Blank/App.xaml.cs
+++ Templates (Project)/Blank/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Windows.ApplicationModel.Activation;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 
 namespace Blank
 {
@@ -16,6 +18,19 @@ namespace Blank
         
         public override Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
         {
+            // This code sets up the title bar
+            // To use the user's chosen accent color by default.
+            // If you want to set your own defaults,
+            // Just update this code to match your chosen colors.
+            // In progress:
+            // Needs good defaults for inactive, button hover, button click colors.
+            // Preferably, these should work well with as many colors as possible.
+            // Maybe find a way to darken the accent color automatically?
+            ApplicationViewTitleBar titlebar = ApplicationView.GetForCurrentView().TitleBar;
+            titlebar.BackgroundColor = (Color)Resources["SystemAccentColor"];
+            titlebar.ForegroundColor = Colors.White;
+            titlebar.ButtonBackgroundColor = (Color)Resources["SystemAccentColor"];
+            titlebar.ButtonForegroundColor = Colors.White;
             NavigationService.Navigate(typeof(Views.MainPage));
             return Task.FromResult<object>(null);
         }
--- Templates (Project)/Minimal/App.xaml.cs
+++ Templates (Project)/Minimal/App.xaml.cs
@@ -3,6 +3,8 @@ using Windows.UI.Xaml;
 using System.Threading.Tasks;
 using Sample.Services.SettingsServices;
 using Windows.ApplicationModel.Activation;
+using Windows.UI.ViewManagement;
+using Windows.UI;
 
 namespace Sample
 {
@@ -27,7 +29,22 @@ namespace Sample
         // runs even if restored from state
         public override Task OnInitializeAsync(IActivatedEventArgs args)
         {
+
             var nav = NavigationServiceFactory(BackButton.Attach, ExistingContent.Include);
+
+            // This code sets up the title bar
+            // To use the user's chosen accent color by default.
+            // If you want to set your own defaults,
+            // Just update this code to match your chosen colors.
+            // In progress:
+            // Needs good defaults for inactive, button hover, button click colors.
+            // Preferably, these should work well with as many colors as possible.
+            // Maybe find a way to darken the accent color automatically?
+            ApplicationViewTitleBar titlebar = ApplicationView.GetForCurrentView().TitleBar;
+            titlebar.BackgroundColor = (Color)Resources["SystemAccentColor"];
+            titlebar.ForegroundColor = Colors.White;
+            titlebar.ButtonBackgroundColor = (Color)Resources["SystemAccentColor"];
+            titlebar.ButtonForegroundColor = Colors.White;
             Window.Current.Content = new Views.Shell(nav);
             return Task.FromResult<object>(null);
         }
