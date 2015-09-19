8882eb6023a26260c8379f407f0994e63cbf0dd9
Nicolò Carandini <n.carandini@outlook.com>
2015-09-19 19:44:42 +0200

    Add TitleBar color customization

--- Templates (Project)/Minimal/Views/Shell.xaml.cs
+++ Templates (Project)/Minimal/Views/Shell.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Template10.Services.NavigationService;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -16,6 +18,7 @@ namespace Sample.Views
             this.InitializeComponent();
             Window = Template10.Common.WindowWrapper.Current();
             MyHamburgerMenu.NavigationService = navigationService;
+            CustomizeTitleBarColors();
         }
 
         public static void SetBusyIndicator(bool busy, string text = null)
@@ -28,5 +31,37 @@ namespace Sample.Views
                 Instance.BusyText.Text = text ?? string.Empty;
             });
         }
+
+        private static void CustomizeTitleBarColors()
+        {
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.BackgroundColor = SetColorFromResources(titleBar.BackgroundColor, "TitleBarBackground");
+            titleBar.ForegroundColor = SetColorFromResources(titleBar.ForegroundColor, "TitleBarForeground");
+            titleBar.ButtonBackgroundColor = SetColorFromResources(titleBar.ButtonBackgroundColor, "TitleBarButtonBackground");
+            titleBar.ButtonForegroundColor = SetColorFromResources(titleBar.ButtonForegroundColor, "TitleBarButtonForeground");
+            titleBar.ButtonHoverBackgroundColor = SetColorFromResources(titleBar.ButtonHoverBackgroundColor, "TitleBarButtonHoverBackground");
+            titleBar.ButtonHoverForegroundColor = SetColorFromResources(titleBar.ButtonHoverForegroundColor, "TitleBarButtonHoverForeground");
+            titleBar.ButtonPressedBackgroundColor = SetColorFromResources(titleBar.ButtonPressedBackgroundColor, "TitleBarButtonPressedBackground");
+            titleBar.ButtonPressedForegroundColor = SetColorFromResources(titleBar.ButtonPressedForegroundColor, "TitleBarButtonPressedForeground");
+            titleBar.ButtonInactiveBackgroundColor = SetColorFromResources(titleBar.ButtonInactiveBackgroundColor, "TitleBarButtonInactiveBackground");
+            titleBar.ButtonInactiveForegroundColor = SetColorFromResources(titleBar.ButtonInactiveForegroundColor, "TitleBarButtonInactiveForeground");
+            titleBar.InactiveBackgroundColor = SetColorFromResources(titleBar.InactiveBackgroundColor, "TitleBarInactiveBackground");
+            titleBar.InactiveForegroundColor = SetColorFromResources(titleBar.InactiveForegroundColor, "TitleBarInactiveForeground");
+        }
+
+        private static Color? SetColorFromResources(Color? actualColor, string resourceName)
+        {
+            try
+            {
+                if (Application.Current.Resources[resourceName] is Color)
+                {
+                    return (Color)Application.Current.Resources[resourceName];
+                }
+            }
+            catch {}
+
+            return actualColor;
+        }
+
     }
 }
