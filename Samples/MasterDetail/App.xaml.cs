79da0dc55e4910d41fd932e396970b8c095fc038
Jerry Nixon <jerry@online.jerrynixon.com>
2015-09-29 21:13:58 -0600

    Introducing MasterDetail

---
+++ Samples/MasterDetail/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+namespace Sample
+{
+    sealed partial class App : Template10.Common.BootStrapper
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        public override Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
+        {
+            NavigationService.Navigate(typeof(Views.MainPage));
+            return Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ Samples/MasterDetail/ViewModels/DetailPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Navigation;
+
+namespace Sample.ViewModels
+{
+    public class DetailPageViewModel : Template10.Mvvm.ViewModelBase
+    {
+        Services.MessageService.MessageService _MessageService;
+
+        public DetailPageViewModel()
+        {
+            if (!Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+                _MessageService = new Services.MessageService.MessageService();
+        }
+
+        public override void OnNavigatedTo(object parameter, NavigationMode mode, IDictionary<string, object> state)
+        {
+            Message = _MessageService.GetMessage(parameter.ToString());
+        }
+
+        Models.Message _Message = default(Models.Message);
+        public Models.Message Message { get { return _Message; } set { Set(ref _Message, value); } }
+    }
+}
