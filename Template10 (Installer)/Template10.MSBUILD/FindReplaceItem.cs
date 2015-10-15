79463f4ac47ddf934d07a063ae8b920dbb34fc0e
Jerry Nixon <jerry@online.jerrynixon.com>
2015-10-15 09:10:25 -0600

    Adding VSIX installer project(s)

---
+++ Template10 (Installer)/Template10.MSBUILD/FindReplaceItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template10.MSBUILD
+{
+    /// <summary>
+    /// Used in matching and replacing.
+    /// </summary>
+    internal class FindReplaceItem
+    {
+        internal string Pattern { get; set; }
+        internal string Replacement { get; set; }
+    }
+}
