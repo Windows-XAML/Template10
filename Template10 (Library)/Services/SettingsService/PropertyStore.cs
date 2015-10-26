1f390895907af6e5c43617929d7cbe6ddaed7e96
Jerry Nixon <jerry@online.jerrynixon.com>
2015-10-26 12:46:19 -0600

    Changed Settings to default to ApplicationDataCompositeValue so the limit can be increased automatically.

--- Template10 (Library)/Services/SettingsService/PropertyStore.cs
+++ Template10 (Library)/Services/SettingsService/PropertyStore.cs
@@ -1,56 +1,57 @@
 ﻿using System;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 
 namespace Template10.Services.SettingsService
 {
-	public class PropertyStore
-	{
-		protected IPropertyMapping Converters { get; }
+    public class PropertyStore
+    {
+        protected IPropertyMapping Converters { get; }
 
-		protected IPropertySet Values { get; }
+        protected IPropertySet Values { get; }
 
-		public PropertyStore(IPropertySet values, IPropertyMapping converters)
-		{
-			if (values == null || converters == null)
-				throw new ArgumentNullException();
-
-			Values = values;
-			Converters = converters;
-		}
+        public PropertyStore(IPropertySet values, IPropertyMapping converters)
+        {
+            if (values == null || converters == null)
+                throw new ArgumentNullException();
+            Values = values;
+            Converters = converters;
+        }
 
         public bool Exists(string key) => Values.ContainsKey(key);
 
         public void Remove(string key)
-		{
-			if (Values.ContainsKey(key))
-				Values.Remove(key);
-		}
-
-		public void Write<T>(string key, T value)
-		{
-			var type = typeof(T);
-			var converter = Converters.GetConverter(type);
-			Values[key] = converter.ToStore(value, type);
-		}
-
-		public T Read<T>(string key, T fallback)
-		{
-			try
-			{
-                // let's not throw an exception when we can easily check this.
-                if (!Values.ContainsKey(key))
-                {
-                    return fallback;
-                }
-
-                var type = typeof(T);
-				var converter = Converters.GetConverter(type);
-				return (T)converter.FromStore(Values[key].ToString(), type);
-
-			} catch
-			{
-				return fallback;
-			}
+        {
+            if (Values.ContainsKey(key))
+                Values.Remove(key);
         }
-	}
+
+        public void Write<T>(string key, T value)
+        {
+            var type = typeof(T);
+            var converter = Converters.GetConverter(type);
+
+            // Why use a composite value container? 
+            // Because it incrases the setting limit to 64k.
+            var container = new ApplicationDataCompositeValue();
+            container["Value"] = converter.ToStore(value, type);
+            Values[key] = container;
+        }
+
+        public T Read<T>(string key, T fallback)
+        {
+            try
+            {
+                if (!Values.ContainsKey(key))
+                    return fallback;
+                var converter = Converters.GetConverter(typeof(T));
+                var container = converter.FromStore(Values[key].ToString(), typeof(T)) as ApplicationDataCompositeValue;
+                return (T)container["Value"];
+            }
+            catch
+            {
+                return fallback;
+            }
+        }
+    }
 }
