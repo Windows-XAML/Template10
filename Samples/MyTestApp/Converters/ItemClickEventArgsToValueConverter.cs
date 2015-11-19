e73911ed9dba37065d949d929c6a1e917b4f3859
dimitar dimitrov <mitkodi@live.com>
2015-11-19 17:42:01 +0200

    Adding MyTestApp

---
+++ Samples/MyTestApp/Converters/ItemClickEventArgsToValueConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace MyTestApp.Converters {
+	class ItemClickEventArgsToValueConverter : IValueConverter {
+		public object Convert(object value, Type targetType, object parameter, string language) {
+			return (value as ItemClickEventArgs)?.ClickedItem;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language) {
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Samples/MyTestApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Template10.Mvvm;
+
+namespace MyTestApp.ViewModels {
+	public class MainPageViewModel : ViewModelBase {
+		#region ICommand TestCommand
+
+		DelegateCommand<object> _testCommand;
+		public ICommand TestCommand {
+			get {
+				return _testCommand ?? (_testCommand = new DelegateCommand<object>(
+					(o) => {
+						System.Diagnostics.Debug.WriteLine(o);
+					}
+				));
+			}
+		}
+
+		#endregion
+	}
+}
