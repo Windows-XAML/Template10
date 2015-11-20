9d8e14fe2521abaf6a553c1486aac3b6686b111e
Vinod Shinde <vinod.shinde@gmail.com>
2015-11-20 14:38:23 -0500

    Changed == to Equals.
    When page parameters are passed, the HamburgerMenu item was not selected
    at startup as the == is not working. So replaced it with Equals.

--- Template10 (Library)/Controls/HamburgerMenu.xaml.cs
+++ Template10 (Library)/Controls/HamburgerMenu.xaml.cs
@@ -82,7 +82,7 @@ namespace Template10.Controls
             pageType = pageType ?? NavigationService.CurrentPageType;
             pageParam = pageParam ?? NavigationService.CurrentPageParam;
             var values = _navButtons.Select(x => x.Value);
-            var button = values.FirstOrDefault(x => x.PageType == pageType && x.PageParameter == pageParam);
+            var button = values.FirstOrDefault(x => x.PageType == pageType && x.PageParameter.Equals(pageParam));
             Selected = button;
         }
 
