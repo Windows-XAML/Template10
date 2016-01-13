0f4cfbfa98dd3719836302600269a1f72137fe44
jerrynixon <jerry@nixoncorp.com>
2016-01-13 10:36:26 -0700

    Adding generic eventargs

--- Template10 (Library)/Common/TypedEventHandler.cs
+++ Template10 (Library)/Common/TypedEventHandler.cs
@@ -3,4 +3,8 @@
 namespace Template10.Common
 {
     public delegate void TypedEventHandler<T>(object sender, T e);
+
+    public class EventArgs<T> : EventArgs { public T Value { get; set; } }
+
+    public class CancelEventArgs<T> : System.ComponentModel.CancelEventArgs { public T Value { get; set; } }
 }
