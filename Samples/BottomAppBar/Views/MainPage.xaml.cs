06a70a0fd5525874e7ea4f6021f9e5a1b9a35a88
DanTheManSWE <dan.o.eriksson@gmail.com>
2016-01-14 08:37:36 +0100

    Update MainPage.xaml.cs
    Removed reference to BottomAppBar.

--- Samples/BottomAppBar/Views/MainPage.xaml.cs
+++ Samples/BottomAppBar/Views/MainPage.xaml.cs
@@ -15,11 +15,9 @@ namespace Sample.Views
 private void SampleClick(object sender, RoutedEventArgs e)
 {
     Shell.SetBusy(true, "Please wait...");
-    BottomAppBar.IsEnabled = false;
     Template10.Common.WindowWrapper.Current().Dispatcher.Dispatch(() =>
     {
         Shell.SetBusy(false);
-        BottomAppBar.IsEnabled = true;
     }, 3000);
 }
     }
