41accf8f55e62b39af3362ae48e29c8e188f651f
jerrynixon <jerry@nixoncorp.com>
2016-01-15 14:36:10 -0700

    Breaking utils into logical class files.

---
+++ Template10 (Library)/Utils/ResourceUtils.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI.Xaml;
+
+namespace Template10.Utils
+{
+    public static class ResourceUtils
+    {
+        public static T GetResource<T>(string resourceName, T otherwise)
+        {
+            try
+            {
+                return (T)Application.Current.Resources[resourceName];
+            }
+            catch
+            {
+                return otherwise;
+            }
+        }
+    }
+}
