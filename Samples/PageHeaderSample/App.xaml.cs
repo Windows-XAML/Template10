2632c31e92f3d5afe65efd3b9cb2e0ea4e2b397a
jerrynixon <jerry@nixoncorp.com>
2016-01-20 13:43:08 -0700

    Simplify PageHeader sample.

--- Samples/PageHeaderSample/App.xaml.cs
+++ Samples/PageHeaderSample/App.xaml.cs
@@ -1,41 +1,16 @@
-using System;
-using Windows.UI.Xaml;
 using System.Threading.Tasks;
-using PageHeaderSample.Services.SettingsServices;
 using Windows.ApplicationModel.Activation;
 
 namespace PageHeaderSample
 {
-	/// Documentation on APIs used in this page:
-	/// https://github.com/Windows-XAML/Template10/wiki
+    /// Documentation on APIs used in this page:
+    /// https://github.com/Windows-XAML/Template10/wiki
 
-	sealed partial class App : Template10.Common.BootStrapper
+    sealed partial class App : Template10.Common.BootStrapper
 	{
-		ISettingsService _settings;
-
 		public App()
 		{
 			InitializeComponent();
-			SplashFactory = (e) => new Views.Splash(e);
-
-			#region App settings
-
-			_settings = SettingsService.Instance;
-			RequestedTheme = _settings.AppTheme;
-			CacheMaxDuration = _settings.CacheMaxDuration;
-			ShowShellBackButton = _settings.UseShellBackButton;
-
-			#endregion
-		}
-
-		// runs even if restored from state
-		public override Task OnInitializeAsync(IActivatedEventArgs args)
-		{
-			// setup hamburger shell
-			var nav = NavigationServiceFactory(BackButton.Attach, ExistingContent.Include);
-			Window.Current.Content = new Views.Shell(nav);
-
-			return Task.CompletedTask;
 		}
 
 		// runs only when not restored from state
@@ -43,7 +18,6 @@ namespace PageHeaderSample
 		{
 			// navigate to first page
 			NavigationService.Navigate(typeof(Views.MainPage));
-
 			return Task.CompletedTask;
 		}
 	}
