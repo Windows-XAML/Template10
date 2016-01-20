a0d5bf659444885d3cc12744e3ebbad4a71c2c4a
Carsten Schuette <c.schuette@neusta.de>
2016-01-20 23:51:04 +0100

    Remove DefaultSerializationService

--- Template10 (Library)/Services/SerializationService/SerializationService.cs
+++ Template10 (Library)/Services/SerializationService/SerializationService.cs
@@ -4,9 +4,6 @@ namespace Template10.Services.SerializationService
 {
     public static class SerializationService
     {
-        private static ISerializationService _default;
-        public static ISerializationService Default => _default ?? (_default = new DefaultSerializationService());
-
         private static ISerializationService _json;
         public static ISerializationService Json => _json ?? (_json = new JsonSerializationService());
     }
