a56e312b136554b295826e53eb01d791b55dec9a
Craig Treasure <craigktreasure@outlook.com>
2016-01-21 15:51:51 -0800

    Fixed #551: Deserialize the navigation parameter.

--- Templates (Project)/Minimal/Views/SettingsPage.xaml.cs
+++ Templates (Project)/Minimal/Views/SettingsPage.xaml.cs
@@ -1,21 +1,20 @@
-﻿using Sample.ViewModels;
-using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
 namespace Sample.Views
 {
-    public sealed partial class SettingsPage : Page
-    {
-        public SettingsPage()
-        {
-            this.InitializeComponent();
-        }
+	public sealed partial class SettingsPage : Page
+	{
+		public SettingsPage()
+		{
+			this.InitializeComponent();
+		}
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            int index;
-            if (int.TryParse(e.Parameter?.ToString(), out index))
-                MyPivot.SelectedIndex = index;
-        }
-    }
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			var index = Template10.Services.SerializationService.SerializationService
+				.Json.Deserialize<int>(e.Parameter?.ToString());
+			MyPivot.SelectedIndex = index;
+		}
+	}
 }
