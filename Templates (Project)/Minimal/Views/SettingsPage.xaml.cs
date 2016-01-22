bfb5550131822c4334067a7e2d7d221c22f153a2
Craig Treasure <craigktreasure@outlook.com>
2016-01-21 16:01:35 -0800

    Removed tabs.

--- Templates (Project)/Minimal/Views/SettingsPage.xaml.cs
+++ Templates (Project)/Minimal/Views/SettingsPage.xaml.cs
@@ -3,18 +3,18 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Sample.Views
 {
-	public sealed partial class SettingsPage : Page
-	{
-		public SettingsPage()
-		{
-			this.InitializeComponent();
-		}
+    public sealed partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            this.InitializeComponent();
+        }
 
-		protected override void OnNavigatedTo(NavigationEventArgs e)
-		{
-			var index = Template10.Services.SerializationService.SerializationService
-				.Json.Deserialize<int>(e.Parameter?.ToString());
-			MyPivot.SelectedIndex = index;
-		}
-	}
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var index = Template10.Services.SerializationService.SerializationService
+                .Json.Deserialize<int>(e.Parameter?.ToString());
+            MyPivot.SelectedIndex = index;
+        }
+    }
 }
