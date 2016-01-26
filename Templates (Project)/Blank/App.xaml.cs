03ce3d67e8c1d73571614c2fc37e669cea98eb11
jerrynixon <jerry@nixoncorp.com>
2016-01-26 10:13:19 -0700

    Spaces

--- Templates (Project)/Blank/App.xaml.cs
+++ Templates (Project)/Blank/App.xaml.cs
@@ -9,15 +9,12 @@ namespace Sample
 
     sealed partial class App : Template10.Common.BootStrapper
     {
-        public App()
-        {
-            InitializeComponent();
-        }
+        public App() { InitializeComponent(); }
 
         public override Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
         {
             NavigationService.Navigate(typeof(Views.MainPage));
-			return Task.CompletedTask;
-		}
+            return Task.CompletedTask;
+        }
     }
 }
