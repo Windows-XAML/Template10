ebe8ff7c5671f16b28f111277b8790a0d0663100
jerrynixon <jerry@nixoncorp.com>
2016-02-03 13:10:20 -0700

    Updating snippets

--- Snippets/InstallSnippets.cmd
+++ Snippets/InstallSnippets.cmd
@@ -6,3 +6,4 @@ for /f "tokens=3*" %%p in ('REG QUERY "HKCU\Software\Microsoft\Windows\CurrentVe
 set SnippetsFolder="%DocumentsFolder%\Visual Studio 2015\Code Snippets\Visual C#\My Code Snippets\"
 set LocalFolder="%~dp0%*T10_*.snippet"
 xcopy.exe %LocalFolder% %SnippetsFolder% /y >nul
+pause
