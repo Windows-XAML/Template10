2c44f146d4cb86ac9aa34115165df530ce14a200
jerrynixon <jerry@nixoncorp.com>
2016-02-12 17:05:54 -0700

    Adding starter for Items template

---
+++ Templates (Items)/Items/App.xaml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+namespace Items
+{
+    /// Documentation on APIs used in this page:
+    /// https://github.com/Windows-XAML/Template10/wiki
+
+    sealed partial class App : Template10.Common.BootStrapper
+    {
+        public App() { InitializeComponent(); }
+
+        public override Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
+        {
+            NavigationService.Navigate(typeof(Views.MainPage));
+            return Task.CompletedTask;
+        }
+    }
+}
+
---
+++ Templates (Items)/Items/Views/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+using System;
+using Windows.UI.Xaml.Controls;
+
+namespace Items.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
