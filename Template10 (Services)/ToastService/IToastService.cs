f04457525c9f51a280f76a3b1f53c865e945df83
Arie Bubberman <ariebubberman@live.nl>
2016-02-15 10:37:31 +0100

    Modified the toastservice 1) Refactored the creation of the Toast into separate classes/methods 2) Added a seperate class for the content of the Toast 3) Added the alttext for the image 4) Fixed issues in the image and text toasts.
    Tested all toast types. ToastText04 and ToastImageAndText04 seem to ignore the provided content and return a default formatted toast. Not sure why..

--- Template10 (Services)/ToastService/IToastService.cs
+++ Template10 (Services)/ToastService/IToastService.cs
@@ -10,10 +10,10 @@ namespace Template10.Services.ToastService
         void ShowToastText02(string title, string content, string arg = null);
         void ShowToastText03(string title, string content, string arg = null);
         void ShowToastText04(string title, string content, string content2, string arg = null);
-        void ShowToastImageAndText01(string image, string content, string arg = null);
-        void ShowToastImageAndText02(string image, string title, string content, string arg = null);
-        void ShowToastImageAndText03(string image, string title, string content, string arg = null);
-        void ShowToastImageAndText04(string image, string title, string content, string content2, string arg = null);
+        void ShowToastImageAndText01(string image,string altText, string content, string arg = null);
+        void ShowToastImageAndText02(string image, string altText, string title, string content, string arg = null);
+        void ShowToastImageAndText03(string image, string altText, string title, string content, string arg = null);
+        void ShowToastImageAndText04(string image, string altText, string title, string content, string content2, string arg = null); 
 
     }
 
