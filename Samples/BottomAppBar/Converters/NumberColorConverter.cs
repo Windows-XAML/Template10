604d5e1c22136bdfc339c9c04d9d42f03ca780be
jerrynixon <jerry@nixoncorp.com>
2016-03-31 01:06:48 -0600

    Added DataGrid look to Sample

---
+++ Samples/BottomAppBar/Converters/NumberColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Utils;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+
+namespace BottomAppBar.Converters
+{
+    public class NumberColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var d = value.ToString().Replace("$", string.Empty).Replace("%", string.Empty).Replace(" ", string.Empty).Replace("(", "-").Replace(")", string.Empty);
+            return double.Parse(d) > 0 ? Colors.Green.ToSolidColorBrush() : Colors.Red.ToSolidColorBrush();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Samples/BottomAppBar/Models/DataItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BottomAppBar.Models
+{
+    public class DataItem
+    {
+        public string Value1 { get; set; }
+        public string Value2 { get; set; }
+        public string Value3 { get; set; }
+        public string Value4 { get; set; }
+        public string Value5 { get; set; }
+        public string Value6 { get; set; }
+        public string Arrow { get; set; }
+    }
+}
