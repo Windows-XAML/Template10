ea8661c283b5be4bfbdbb3aa09d3f154f80c2ee6
Zinkevich Viachaslau <viachaslau.zinkevich@outlook.com>
2016-05-10 13:31:12 +0300

    Added ViewService for secondary view safe creation and closing management. Added Example project "MultipleViews" to show ViewService usage.

---
+++ Samples/MultipleViews/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Template10.Mvvm;
+using Template10.Services.NavigationService;
+using Windows.UI.Xaml.Navigation;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using Sample.Views;
+using Template10.Common;
+using Template10.Services.ViewService;
+
+namespace Sample.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        private readonly WindowWrapper wrapper = WindowWrapper.Current();
+        public IList<string> Messages { get; }=new ObservableCollection<string>();
+        public ICommand OpenViewCommand { get; }
+
+        public MainPageViewModel()
+        {
+            OpenViewCommand = new RelayCommand(OpenView);
+        }
+
+        private async void OpenView()
+        {
+            var service = new ViewService();
+            var control = await service.OpenAsync(typeof (SecondaryPage), null, Guid.NewGuid().ToString());
+            control.Released += Control_Released;
+        }
+
+        private void Control_Released(object sender, EventArgs e)
+        {
+            Window.Current.Close();
+            var control = (ViewLifetimeControl) sender;
+            PostMessage(new GenericMessage<string>($"{DateTime.Now} - {control.Id}: Released"));
+        }
+
+        public override Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
+        {
+            MessengerInstance.Register<GenericMessage<string>>(this, PostMessage);
+            return base.OnNavigatedToAsync(parameter, mode, state);
+        }
+
+        private void PostMessage(GenericMessage<string> message)
+        {
+            wrapper.Dispatcher.Dispatch(() => Messages.Add(message.Content));
+        }
+    }
+}
---
+++ Template10 (Library)/Services/ViewService/ViewService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Template10.Common;
+using static Template10.Services.LoggingService.LoggingService;
+
+namespace Template10.Services.ViewService
+{
+    public class ViewService:IViewService
+    {
+        /// <summary>
+        /// Should be called as soon as possible. The best place is <see cref="Application"/> constructor
+        /// </summary>
+        /// <param name="bootStrapper"><see cref="BootStrapper"/> used as current appliction. If <value>null</value> the <see cref="BootStrapper.Current"/> will be used</param>
+        public static void Initialize(BootStrapper bootStrapper=null)
+        {
+            if(bootStrapper == null)
+                bootStrapper = BootStrapper.Current;
+            bootStrapper.WindowCreated += OnWindowCreated;
+        }
+
+        private static void OnWindowCreated(object sender, WindowCreatedEventArgs windowCreatedEventArgs)
+        {
+            var view = CoreApplication.GetCurrentView();
+            if (!view.IsMain && !view.IsHosted)
+            {
+                var control = ViewLifetimeControl.GetForCurrentView();
+                //This one time it should be made manually, as after Consolidate event fires the inner reference number should become zero
+                control.StartViewInUse();
+                //This is necessary to not make control.StartViewInUse()/control.StopViewInUse() manually on each and every async call. Facade will do it for you
+                SynchronizationContext.SetSynchronizationContext(new SecondaryViewSynchronizationContextFacade(control,
+                    SynchronizationContext.Current));
+            }
+        }
+
+        public async Task<ViewLifetimeControl> OpenAsync(Type page, object parameter = null, string title = null,
+            ViewSizePreference size = ViewSizePreference.UseHalf)
+        {
+            WriteLine($"Page: {page}, Parameter: {parameter}, Title: {title}, Size: {size}");
+
+            var currentView = ApplicationView.GetForCurrentView();
+            title = title ?? currentView.Title;
+           
+            var newView = CoreApplication.CreateNewView();
+            var dispatcher = new DispatcherWrapper(newView.Dispatcher);
+            var newControl = await dispatcher.Dispatch(async () =>
+            {                
+                var control=ViewLifetimeControl.GetForCurrentView();
+                var newWindow = Window.Current;
+                var newAppView = ApplicationView.GetForCurrentView();
+                newAppView.Title = title;                
+                var nav = BootStrapper.Current.NavigationServiceFactory(BootStrapper.BackButton.Ignore, BootStrapper.ExistingContent.Exclude);
+                nav.Navigate(page, parameter);
+                newWindow.Content = nav.Frame;
+                newWindow.Activate();
+
+                await ApplicationViewSwitcher
+                    .TryShowAsStandaloneAsync(newAppView.Id, ViewSizePreference.Default, currentView.Id, size);
+                return control;
+            });
+            return newControl;
+        }
+    }
+}
