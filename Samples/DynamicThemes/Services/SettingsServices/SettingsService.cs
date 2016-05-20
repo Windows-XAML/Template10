3c8906c8649b23d8b074a4c3bce96282e59e2bad
dg2k <dg2k@apps4smartdevices.com>
2016-05-20 21:14:33 +0100

    Sample project showing setting of themes defined in custom.xaml during run time.

---
+++ Samples/DynamicThemes/Services/SettingsServices/SettingsService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Template10.Common;
+using Template10.Utils;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Sample.Services.SettingsServices
+{
+    public class SettingsService
+    {
+        public static SettingsService Instance { get; } = new SettingsService();
+        Template10.Services.SettingsService.ISettingsHelper _helper;
+        private SettingsService()
+        {
+            _helper = new Template10.Services.SettingsService.SettingsHelper();
+        }
+
+        public ApplicationTheme AppTheme
+        {
+            get
+            {
+                var theme = ApplicationTheme.Dark;
+                var value = _helper.Read<string>(nameof(AppTheme), theme.ToString());
+                return Enum.TryParse<ApplicationTheme>(value, out theme) ? theme : ApplicationTheme.Dark;
+            }
+            set
+            {
+                _helper.Write(nameof(AppTheme), value.ToString());
+                (Window.Current.Content as FrameworkElement).RequestedTheme = value.ToElementTheme();
+                Views.Shell.SetRequestedTheme(value, UseBackgroundChecked);
+            }
+        }
+
+         public SplitViewDisplayMode CompactDisplayMode
+        {
+            get
+            {
+                var mode = SplitViewDisplayMode.CompactOverlay;
+                var value = _helper.Read<string>(nameof(CompactDisplayMode), SplitViewDisplayMode.CompactOverlay.ToString());
+                return Enum.TryParse<SplitViewDisplayMode>(value, out mode) ? mode : SplitViewDisplayMode.CompactInline;
+            }
+            set
+            {
+                _helper.Write(nameof(CompactDisplayMode), value.ToString());
+                Views.Shell.HamburgerMenu.DisplayMode = value;
+                Views.Shell.HamburgerMenu.VisualStateNormalDisplayMode = Views.Shell.HamburgerMenu.VisualStateWideDisplayMode = value;
+
+                if (value == SplitViewDisplayMode.CompactOverlay)
+                {
+                    Views.Shell.HamburgerMenu.IsOpen = false;}
+                else
+                {
+                    Views.Shell.HamburgerMenu.IsOpen = true;
+                }
+            }
+        }
+
+        public bool UseBackgroundChecked
+        {
+            get { return _helper.Read<bool>(nameof(UseBackgroundChecked), true); }
+            set
+            {
+                _helper.Write(nameof(UseBackgroundChecked), value);
+                Views.Shell.SetRequestedTheme(AppTheme, value);
+            }
+        }
+        
+
+        public TimeSpan CacheMaxDuration
+        {
+            get { return _helper.Read<TimeSpan>(nameof(CacheMaxDuration), TimeSpan.FromDays(2)); }
+            set
+            {
+                _helper.Write(nameof(CacheMaxDuration), value);
+                BootStrapper.Current.CacheMaxDuration = value;
+            }
+        }
+    }
+}
---
+++ Samples/DynamicThemes/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+using Template10.Services.NavigationService;
+using Windows.UI.Xaml.Navigation;
+
+namespace Sample.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel()
+        {
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                Value = "Designtime value";
+            }
+        }
+
+        string _Value = "Gas";
+        public string Value { get { return _Value; } set { Set(ref _Value, value); } }
+
+        public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> suspensionState)
+        {
+            if (suspensionState.Any())
+            {
+                Value = suspensionState[nameof(Value)]?.ToString();
+            }
+            await Task.CompletedTask;
+        }
+
+        public override async Task OnNavigatedFromAsync(IDictionary<string, object> suspensionState, bool suspending)
+        {
+            if (suspending)
+            {
+                suspensionState[nameof(Value)] = Value;
+            }
+            await Task.CompletedTask;
+        }
+
+        public override async Task OnNavigatingFromAsync(NavigatingEventArgs args)
+        {
+            args.Cancel = false;
+            await Task.CompletedTask;
+        }
+
+
+        public void GotoSettings() =>
+            NavigationService.Navigate(typeof(Views.SettingsPage), 0);
+
+        public void GotoPrivacy() =>
+            NavigationService.Navigate(typeof(Views.SettingsPage), 1);
+
+        public void GotoAbout() =>
+            NavigationService.Navigate(typeof(Views.SettingsPage), 2);
+
+    }
+}
