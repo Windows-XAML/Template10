5acdf6c9b9246be391bbe57795ac7b83f157b44e
Zinkevich Viachaslau <viachaslau.zinkevich@outlook.com>
2016-05-22 17:27:40 +0300

    Added interface and adapter to be allow update commands from same interface without necessity of manual check and cast to specific types.

---
+++ Template10 (Library)/Mvvm/ChangedCommandAdapter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Template10.Mvvm
+{
+    /// <summary>
+    /// Use this to adapt any <see cref="ICommand"/> to <see cref="IChangedCommand"/> with posibility to manually initiate 
+    /// <see cref="CanExecuteChanged"/>
+    /// </summary>
+    public class ChangedCommandAdapter:IChangedCommand
+    {
+        private readonly ICommand command;
+        private readonly Action raiseCanExecuteChangedAction;
+
+        public ChangedCommandAdapter(ICommand command, Action raiseCanExecuteChangedAction)
+        {
+            if(command == null)
+                throw new ArgumentNullException(nameof(command));
+            this.command = command;
+            this.raiseCanExecuteChangedAction = raiseCanExecuteChangedAction;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return command.CanExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            command.Execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { command.CanExecuteChanged += value; }
+            remove { command.CanExecuteChanged -= value; }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            raiseCanExecuteChangedAction?.Invoke();
+        }
+    }
+}
