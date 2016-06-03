664c3a83185330460529f280549de63e58544809
Viachaslau-Zinkevich <viachaslau.zinkevich@outlook.com>
2016-06-04 01:08:39 +0300

    Added interface and adapter to be allow update commands from same interface without necessity of manual check and cast to specific types. (#1001)
    Add an optional extended description…

---
+++ Template10 (Library)/Mvvm/IChangedCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Input;
+
+namespace Template10.Mvvm
+{
+    /// <summary>
+    /// Extension of <see cref="ICommand"/> to allow manually call of <see cref="ICommand.CanExecuteChanged"/>
+    /// </summary>
+    public interface IChangedCommand:ICommand
+    {
+        void RaiseCanExecuteChanged();
+    }
+}
