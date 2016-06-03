01481565a6cbdd2f90cc86851b024fe6f9db18aa
dg2k <dg2k@apps4smartdevices.com>
2016-06-03 17:10:07 +0100

    Sample project showing setting of themes defined in custom.xaml during run time. (#998)

---
+++ Samples/DynamicThemes/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using Sample.Services.SettingsServices;
+using Template10.Controls;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Sample
+{
+    /// Documentation on APIs used in this page:
+    /// https://github.com/Windows-XAML/Template10/wiki
+
+    [Bindable]
+    sealed partial class App : Template10.Common.BootStrapper
+    {
+        public App()
+        {
+            InitializeComponent();
+            SplashFactory = (e) => new Views.Splash(e);
+
+            #region App settings
+
+            var _settings = SettingsService.Instance;
+            RequestedTheme = _settings.AppTheme;
+            CacheMaxDuration = _settings.CacheMaxDuration;
+            ShowShellBackButton = false;
+
+            #endregion
+        }
+
+        public override async Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+            if (Window.Current.Content as ModalDialog == null)
+            {
+                // create a new frame 
+                var nav = NavigationServiceFactory(BackButton.Attach, ExistingContent.Include);
+
+                // create modal root
+                Window.Current.Content = new ModalDialog
+                {
+                    DisableBackButtonWhenModal = true,
+                    Content = new Views.Shell(nav),
+                    ModalContent = new Views.Busy(),
+                };
+            }
+            await Task.CompletedTask;
+        }
+
+        public override async Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
+        {
+            // long-running startup tasks go here
+            await Task.Delay(500);
+
+            NavigationService.Navigate(typeof(Views.MainPage));
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ Samples/DynamicThemes/Views/SupportPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml.Controls;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Sample.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SupportPage : Page
+    {
+        public SupportPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
