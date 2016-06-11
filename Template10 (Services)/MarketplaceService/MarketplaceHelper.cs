c077ff89bca2c633b3758728eb64686b3cb233b8
Don Kackman <dkackman@gmail.com>
2016-06-11 16:16:30 -0500

    Add MarketplaceService to address issue #1048 (#1049)
    * add basic MarketplaceService
    
    * use C# 6 lambda invocation syntax to better match other services

--- Template10 (Services)/MarketplaceService/MarketplaceHelper.cs
+++ Template10 (Services)/MarketplaceService/MarketplaceHelper.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Windows.System;
+
 namespace Template10.Services.MarketplaceService
 {
+    //https://msdn.microsoft.com/en-us/windows/uwp/launch-resume/launch-store-app
     public class MarketplaceHelper
     {
-        public void LaunchAppInStore()
+        public async Task LaunchAppInStoreByProductFamilyName(string pfn)
         {
+            await Launcher.LaunchUriAsync(new Uri($"ms-windows-store:pdp?PFN={Uri.EscapeUriString(pfn)}"));
+        }
 
+        public async Task LaunchAppInStoreByAppId(string productId)
+        {
+            await Launcher.LaunchUriAsync(new Uri($"ms-windows-store:pdp?ProductId={Uri.EscapeUriString(productId)}"));
+        }
+
+        public async Task LaunchAppReviewInStoreByProductFamilyName(string pfn)
+        {
+            await Launcher.LaunchUriAsync(new Uri($"ms-windows-store:review?PFN={Uri.EscapeUriString(pfn)}"));
+        }
+
+        public async Task LaunchAppReviewInStoreByAppId(string productId)
+        {
+            await Launcher.LaunchUriAsync(new Uri($"ms-windows-store:review?ProductId={Uri.EscapeUriString(productId)}"));
+        }
+
+        public async Task LaunchPublisherPageInStore(string publisherName)
+        {
+            await Launcher.LaunchUriAsync(new Uri($"ms-windows-store:publisher?name={Uri.EscapeUriString(publisherName)}"));
         }
     }
 }
