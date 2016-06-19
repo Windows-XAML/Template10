ac0bfa360ccaea60a1298d9693c5bbd35444195a
Don Kackman <dkackman@gmail.com>
2016-06-18 22:04:16 -0500

    fix duration check

---
+++ Samples/Marketplace/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using System.Threading.Tasks;
+
+using Template10.Common;
+using Template10.Services.MarketplaceService;
+
+namespace Template10.Samples.Marketplace
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : BootStrapper
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        public override Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+            return base.OnInitializeAsync(args);
+        }
+
+        public override async Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
+        {
+            await NavigationService.NavigateAsync(typeof(Views.MainPage));
+
+            if (startKind == StartKind.Launch)
+            {
+                var nagService = new AppNagService();
+                await nagService.RegisterAppReviewNag(5, TimeSpan.FromDays(1));
+            }
+        }
+    }
+}
---
+++ Samples/Marketplace/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Template10.Samples.Marketplace.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
