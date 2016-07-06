c5ad301f560d783a2d5adef8ae62ee88940ffecd
Don Kackman <dkackman@gmail.com>
2016-07-06 16:38:51 -0500

    remove the message hashed id. it was a bad idea

--- Template10 (Services)/NagService/Nag.cs
+++ Template10 (Services)/NagService/Nag.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 
-using Windows.Security.Cryptography;
-using Windows.Security.Cryptography.Core;
-
 namespace Template10.Services.NagService
 {
     public class Nag
     {
-        public Nag(string message, Action nagAction)
-            : this(HashText(message), message, nagAction)
-        {
-        }
-
         public Nag(string id, string message, Action nagAction)
         {
             if (string.IsNullOrEmpty(id)) throw new ArgumentException("id cannot be null or empty", "id");
@@ -38,19 +30,5 @@ namespace Template10.Services.NagService
         public string AcceptText { get; set; } = "Yes";
 
         public string DeferText { get; set; } = "Not now";
-
-        private static string HashText(string text)
-        {
-            if (string.IsNullOrEmpty(text))
-            {
-                return string.Empty;
-            }
-
-            var buffer = CryptographicBuffer.ConvertStringToBinary(text, BinaryStringEncoding.Utf8);
-            var hashAlgorithmProvider = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Sha1);
-            var bufferHash = hashAlgorithmProvider.HashData(buffer);
-
-            return CryptographicBuffer.EncodeToHexString(bufferHash);
-        }
     }
 }
