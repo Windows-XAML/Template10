d9b9f6acf85eb3748b08c8c16c1c4a7ef6f89a1b
Don Kackman <dkackman@gmail.com>
2016-07-06 20:07:27 -0500

    code comments

--- Template10 (Services)/NagService/Nag.cs
+++ Template10 (Services)/NagService/Nag.cs
@@ -2,8 +2,17 @@
 
 namespace Template10.Services.NagService
 {
+    /// <summary>
+    /// Information about a nag
+    /// </summary>
     public class Nag
     {
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="id"><see cref="Nag.Id"/></param>
+        /// <param name="message"><see cref="Nag.Message"/></param>
+        /// <param name="nagAction"><see cref="Nag.NagAction"/></param>
         public Nag(string id, string message, Action nagAction)
         {
             if (string.IsNullOrEmpty(id)) throw new ArgumentException("id cannot be null or empty", "id");
@@ -15,20 +24,45 @@ namespace Template10.Services.NagService
             NagAction = nagAction;
         }
 
+        /// <summary>
+        /// The unique id for this nag
+        /// Must be unique within the context of an app
+        /// </summary>
         public string Id { get; private set; }
 
+        /// <summary>
+        /// The message to nag the user with
+        /// </summary>
         public string Message { get; private set; }
 
+        /// <summary>
+        /// The action to perform if the user accepts the nag
+        /// </summary>
         public Action NagAction { get; private set; }
 
+        /// <summary>
+        /// The title of the nag to display
+        /// </summary>
         public string Title { get; set; }
 
+        /// <summary>
+        /// Flag indicating if the user can defer the <see cref="NagAction"/> until later
+        /// </summary>
         public bool AllowDefer { get; set; }
 
+        /// <summary>
+        /// The text to display on the decline button
+        /// </summary>
         public string DeclineText { get; set; } = "No, thanks";
 
+        /// <summary>
+        /// The text to display on the accept button
+        /// </summary>
         public string AcceptText { get; set; } = "Yes";
 
+        /// <summary>
+        /// The text to display on the defer button
+        /// </summary>
         public string DeferText { get; set; } = "Not now";
     }
 }
