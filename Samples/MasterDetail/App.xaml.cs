95b9d4662dd192482d673e304b271f356dc26f52
jerrynixon <jerry@nixoncorp.com>
2016-07-12 16:22:52 -0600

    Adding nested items demo in HamMenu

--- Samples/MasterDetail/App.xaml.cs
+++ Samples/MasterDetail/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
 using Windows.ApplicationModel.Activation;
 using Template10.Samples.MasterDetailSample.Views;
+using Windows.UI.Xaml;
+using Template10.Controls;
 
 namespace Template10.Samples.MasterDetailSample
 {
@@ -9,6 +11,16 @@ namespace Template10.Samples.MasterDetailSample
         public App()
         {
             InitializeComponent();
+        }
+
+        public override UIElement CreateRootElement(IActivatedEventArgs e)
+        {
+            var service = NavigationServiceFactory(BackButton.Attach, ExistingContent.Exclude);
+            return new ModalDialog
+            {
+                DisableBackButtonWhenModal = true,
+                Content = new Views.Shell(service),
+            };
         }
 
         public override Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
