c8c4e4dd131366bc87f3c20894cfbe5c0945f7b1
jerrynixon <jerry@nixoncorp.com>
2016-09-21 18:03:11 -0600

    Add user control to sample

---
+++ Samples/MultiplePageHeaders/Controls/MyUserControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MultiplePageHeaders.Controls
+{
+    public sealed partial class MyUserControl : UserControl
+    {
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+
+        static int counter = 0;
+
+        private async void Frame_Loaded(object sender, RoutedEventArgs e)
+        {
+            var frame = sender as Frame;
+            var current = Template10.Common.BootStrapper.Current;
+            var nav = current.NavigationServiceFactory(Template10.Common.BootStrapper.BackButton.Ignore, Template10.Common.BootStrapper.ExistingContent.Exclude, frame);
+            await nav.NavigateAsync(typeof(Views.DetailPage), $"Frame {++counter}");
+        }
+    }
+}
--- Samples/MultiplePageHeaders/Views/ContainerPage.xaml.cs
+++ Samples/MultiplePageHeaders/Views/ContainerPage.xaml.cs
@@ -7,23 +7,7 @@ namespace MultiplePageHeaders.Views
     {
         public ContainerPage()
         {
-            this.InitializeComponent();
-        }
-
-        private async void Frame1_Loaded(object sender, RoutedEventArgs e)
-        {
-            var frame = sender as Frame;
-            var current = Template10.Common.BootStrapper.Current;
-            var nav = current.NavigationServiceFactory(Template10.Common.BootStrapper.BackButton.Ignore, Template10.Common.BootStrapper.ExistingContent.Exclude, frame);
-            await nav.NavigateAsync(typeof(DetailPage), "Frame 1");
-        }
-
-        private async void Frame2_Loaded(object sender, RoutedEventArgs e)
-        {
-            var frame = sender as Frame;
-            var current = Template10.Common.BootStrapper.Current;
-            var nav = current.NavigationServiceFactory(Template10.Common.BootStrapper.BackButton.Ignore, Template10.Common.BootStrapper.ExistingContent.Exclude, frame);
-            await nav.NavigateAsync(typeof(DetailPage), "Frame 2");
+            InitializeComponent();
         }
     }
 }
