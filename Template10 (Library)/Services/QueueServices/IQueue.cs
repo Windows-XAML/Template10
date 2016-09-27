4dd19f73e8ff1f7040ee3b271532779e54bfe2e0
Callum Moffat <callum90ish@gmail.com>
2016-09-28 09:17:22 +1300

    Added QueueService (resolves #1151)

---
+++ Template10 (Library)/Services/QueueServices/IQueue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Common;
+
+namespace Template10.Services.QueueServices
+{
+    public interface IQueue<T>
+    {
+        #region Events
+        event TypedEventHandler<T> Enqueued;
+        event TypedEventHandler<T> Dequeued;
+        #endregion
+        void Enqueue(T item);
+        T Dequeue();
+    }
+}
