a990fe58f8b2319b5ec577d5bb8b8871c2be16e4
Carsten Schuette <schuettecarsten@googlemail.com>
2016-10-21 16:27:33 +0200

    The synchronous implementation of Dispatch should not be async (#1279)
    * The synchronous implementation of Dispatch should not be async
    
    * Fix Task.Delay(delayms).ConfigureAwait() calls to stay in the correct thread context
    
    * Add two missing methods to the interface
    
    * Fix blank lines

--- Template10 (Library)/Common/IDispatcherWrapper.cs
+++ Template10 (Library)/Common/IDispatcherWrapper.cs
@@ -8,11 +8,13 @@ namespace Template10.Common
     public interface IDispatcherWrapper
     {
         void Dispatch(Action action, int delayms = 0, CoreDispatcherPriority priority = CoreDispatcherPriority.Normal);
+        T Dispatch<T>(Func<T> action, int delayms = 0, CoreDispatcherPriority priority = CoreDispatcherPriority.Normal);
         Task DispatchAsync(Func<Task> func, int delayms = 0, CoreDispatcherPriority priority = CoreDispatcherPriority.Normal);
         Task DispatchAsync(Action action, int delayms = 0, CoreDispatcherPriority priority = CoreDispatcherPriority.Normal);
         Task<T> DispatchAsync<T>(Func<T> func, int delayms = 0, CoreDispatcherPriority priority = CoreDispatcherPriority.Normal);
 
         void DispatchIdle(Action action, int delayms = 0);
+        T DispatchIdle<T>(Func<T> action, int delayms = 0) where T : class;
         Task DispatchIdleAsync(Func<Task> func, int delayms = 0);
         Task DispatchIdleAsync(Action action, int delayms = 0);
         Task<T> DispatchIdleAsync<T>(Func<T> func, int delayms = 0);
