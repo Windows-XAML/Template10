91d4e663616652356f84e875402aa366f55936d1
jerrynixon <Jerry@nixoncorp.com>
2017-04-22 11:07:28 -0600

    Arranging files

---
+++ Services/Template10.Services.WebApiService/Service/Services.WebApiService/WebApiService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+
+namespace Template10.Services.WebApiService
+{
+    public class WebApiService : IWebApiService
+    {
+        WebApiHelper _helper = new WebApiHelper();
+
+        public string Serialize<T>(T item) => _helper.Serialize<T>(item);
+
+        public T Deserialize<T>(string json) => _helper.Deserialize<T>(json);
+
+        public Task<string> GetAsync(Uri path) => _helper.GetAsync(path);
+
+        public Task<HttpResponseMessage> PutAsync<T>(Uri path, T payload) => _helper.PutAsync<T>(path, payload);
+
+        public Task<HttpResponseMessage> PostAsync<T>(Uri path, T payload) => _helper.PostAsync(path, payload);
+
+        public Task<HttpResponseMessage> DeleteAsync(Uri path) => _helper.DeleteAsync(path);
+    }
+}
