ea3f14e21d69c2163f5cb22f2d443ee8b2c83f1e
Callum Moffat <callum90ish@outlook.com>
2017-05-23 16:43:21 +1200

    Updated

---
+++ Independent.Services/Template10.Services.SerializationService/Demo/Services/PeopleService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Services.SerializationService;
+
+namespace SerializationService.Demo.Services
+{
+    public sealed class PeopleService
+    {
+        public static PeopleService Instance { get; } =
+            new PeopleService();
+
+        private PeopleService()
+        {
+
+        }
+
+        private static ISerializationService SerializationService { get; } =
+            Template10.Services.SerializationService.SerializationService.Json;
+
+        public string SerializePerson(Models.Person person)
+        {
+            return SerializationService.Serialize(person);
+        }
+
+        public Models.Person DeserializePerson(string json)
+        {
+            return SerializationService.Deserialize<Models.Person>(json);
+        }
+    }
+}
