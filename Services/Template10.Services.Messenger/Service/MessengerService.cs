4d8d03da2dc5b2bec5272db6163565e6737efbaa
jerrynixon <Jerry@nixoncorp.com>
2017-08-25 11:39:49 -0600

    Adding logging to Messaging

--- Services/Template10.Services.Messenger/Service/MessengerService.cs
+++ Services/Template10.Services.Messenger/Service/MessengerService.cs
@@ -9,9 +9,28 @@ namespace Template10.Services.Messenger
             _adapter = adapter;
         }
         IMessengerAdapter _adapter { get; set; }
-        public void Send<T>(T message) => _adapter.Send(message);
+        public void Send<T>(T message)
+        {
+            LogThis<T>();
+            _adapter.Send(message);
+        }
         public void Subscribe<T>(object subscriber, Action<T> callback) => _adapter.Subscribe(subscriber, callback);
         public void Unsubscribe<T>(object subscriber, Action<T> callback) => _adapter.Unsubscribe(subscriber, callback);
         public void Unsubscribe(object subscriber) => _adapter.Unsubscribe(subscriber);
+
+        private static void LogThis<T>()
+        {
+            var message = $"{nameof(MessengerService)}.Send({typeof(T)})";
+            try
+            {
+                var container = Container.ContainerService.Default;
+                var logger = container.Resolve<Logging.ILoggingService>();
+                logger.WriteLine(message);
+            }
+            catch (Exception)
+            {
+                System.Diagnostics.Debug.WriteLine($"LOGGING> {message}");
+            }
+        }
     }
 }
