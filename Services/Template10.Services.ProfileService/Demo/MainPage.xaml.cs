a215c6dd18841836e6dafb5c4d3b1030138f36d6
Jerry Nixon <jerry@nixoncorp.com>
2017-09-01 09:33:41 -0600

    Fixing the Profile service

---
+++ Services/Template10.Services.ProfileService/Demo/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Demo
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var service = new Template10.Services.Profile.UserService();
+            var user = await service.GetUserExAsync();
+            DataContext = user;
+        }
+    }
+}
---
+++ Services/Template10.Services.ProfileService/Service/Profile/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
+
+namespace Template10.Services.Profile
+{
+    public interface IUserService
+    {
+        Task<IUserEx> GetUserExAsync();
+    }
+}
