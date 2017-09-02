0526c111ada2c92836d27494948adba310d2453c
Jerry Nixon <jerry@nixoncorp.com>
2017-09-02 16:07:02 -0600

    Forwading more settings into Template10.Settings

--- Template10/Template10.Core/Settings.cs
+++ Template10/Template10.Core/Settings.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using Template10.Core;
 using Template10.Services.Gesture;
+using Template10.Strategies;
 using Windows.UI.Xaml;
 
 namespace Template10
 {
     public static partial class Settings
     {
+        // navigation settings
+
         public static ElementTheme DefaultTheme
         {
             get => Navigation.Settings.DefaultTheme;
@@ -18,6 +21,52 @@ namespace Template10
             get => Navigation.Settings.CacheMaxDuration;
             set => Navigation.Settings.CacheMaxDuration = value;
         }
+
+        public static bool RequireSerializableParameters
+        {
+            get => Navigation.Settings.RequireSerializableParameters;
+            set => Navigation.Settings.RequireSerializableParameters = value;
+        }
+
+        // strategy settings
+
+        public static NetworkRequirements NetworkRequirement
+        {
+            get => Strategies.Settings.NetworkRequirement;
+            set => Strategies.Settings.NetworkRequirement = value;
+        }
+
+        public static bool ShowExtendedSplashScreen
+        {
+            get => Strategies.Settings.ShowExtendedSplashScreen;
+            set => Strategies.Settings.ShowExtendedSplashScreen = value;
+        }
+
+        public static bool AppAlwaysResumes
+        {
+            get => Strategies.Settings.AppAlwaysResumes;
+            set => Strategies.Settings.AppAlwaysResumes = value;
+        }
+
+        public static bool EnableCustomTitleBar
+        {
+            get => Strategies.Settings.EnableCustomTitleBar;
+            set => Strategies.Settings.EnableCustomTitleBar = value;
+        }
+
+        public static bool EnableExtendedSessionStrategy
+        {
+            get => Strategies.Settings.EnableExtendedSessionStrategy;
+            set => Strategies.Settings.EnableExtendedSessionStrategy = value;
+        }
+
+        public static bool EnableLifecycleStrategy
+        {
+            get => Strategies.Settings.EnableLifecycleStrategy;
+            set => Strategies.Settings.EnableLifecycleStrategy = value;
+        }
+
+        // services settings
 
         public static ShellBackButtonPreferences ShellBackButtonPreference
         {
