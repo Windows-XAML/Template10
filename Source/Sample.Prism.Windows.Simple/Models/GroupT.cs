50b83328c57a2e4ff667f71443449998a4c39206
Jerry Nixon <jerry@nixoncorp.com>
2018-09-13 21:11:03 -0600

    Added Simple sample

---
+++ Source/Sample.Prism.Windows.Simple/Models/GroupT.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Sample.Models
+{
+    public class GroupT<T>
+    {
+        public string Title { get; set; }
+        public IEnumerable<T> Items { get; set; }
+    }
+}
