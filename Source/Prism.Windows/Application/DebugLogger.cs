9f2d699aab50c2cfa8f1202ebd93f01716c73f34
Jerry Nixon <jerry@nixoncorp.com>
2018-09-16 16:14:03 -0600

    Adding a better default logger.

---
+++ Source/Prism.Windows/Application/DebugLogger.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Logging;
+using System.Diagnostics;
+
+namespace Prism
+{
+    public class DebugLogger : ILoggerFacade
+    {
+        public void Log(string message, Category category, Priority priority)
+        {
+            Debug.WriteLine($"{priority} {category} {message}");
+        }
+    }
+}
--- Source/Prism.Windows/Application/PrismApplicationBase.core.cs
+++ Source/Prism.Windows/Application/PrismApplicationBase.core.cs
@@ -17,6 +17,31 @@ namespace Prism
 {
     public interface IPrismApplicationBase
     {
+        /*
+         
+         STARTUP EXECUTION ORDER
+         * 01. PrismApplicationBase.InternalInitialize
+         * 02. PrismApplicationBase.RegisterRequiredTypes
+         * 03. PrismApplicationBase.InternalStartAsync(Args:Windows.ApplicationModel.Activation.LaunchActivatedEventArgs Kind:Launch Cause:PrimaryTile)
+         * 04. PrismApplicationBase.CallOnInitializedOnce
+          
+         THEN
+         * 05. NavigationService.NavigateAsync(MainPage)
+         * 06. FrameFacade.NavigateAsync(MainPage)
+         * 07. FrameFacade.NavigateAsync(MainPage)
+         * 08. [From]View-Model is null.
+         * 09. FrameFacade.NavigateFrameAsync HasThreadAccess: True
+         * 10. FrameFacade.OrchestrateAsync.NavigateFrameAsync() returned True.
+         * 11. Calling OnNavigatedToAsync
+         * 12. INavigatedAwareAsync.OnNavigatedToAsync() called.
+         * 13. Calling OnNavigatingTo
+         * 14. INavigatingAware not implemented.
+         * 15. Calling OnNavigatedTo
+         * 16. INavigatedAware not implemented.
+         
+         */
+
+
         IContainerProvider Container { get; }
         void ConfigureViewModelLocator();
         IContainerExtension CreateContainer();
@@ -86,7 +111,7 @@ namespace Prism
         {
             // don't forget there is no logger yet
             Debug.WriteLine($"{nameof(PrismApplicationBase)}.{nameof(CallOnInitializedOnce)}");
-            
+
             // once and only once, ever
             if (Interlocked.Increment(ref _initialized) == 1)
             {
@@ -187,7 +212,7 @@ namespace Prism
 
             // standard prism services
 
-            container.RegisterSingleton<ILoggerFacade, EmptyLogger>();
+            container.RegisterSingleton<ILoggerFacade, DebugLogger>();
             container.RegisterSingleton<IEventAggregator, EventAggregator>();
         }
 
