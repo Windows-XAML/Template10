537306cb9b37199382fef60fd361e976e9cc4bf5
Daren May <daren.may@gmail.com>
2019-09-26 11:11:21 -0600

    Splitting Extras Started work

---
+++ Source/Template10.Service.Compression/Abstractions/ICompressionService.cs
@@ -0,0 +1,8 @@
+﻿namespace Template10.Services.Compression
+{
+    public interface ICompressionService
+    {
+        string Unzip(string value, CompressionMethods method = CompressionMethods.gzip);
+        string Zip(string value, CompressionMethods method = CompressionMethods.gzip);
+    }
+}
---
+++ Source/Template10.Service.Compression/Default/CompressionService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template10.Services.Compression
+{
+    public class CompressionService : ICompressionService
+    {
+        public string Zip(string value, CompressionMethods method = CompressionMethods.gzip)
+        {
+            var buffer = Encoding.UTF8.GetBytes(value);
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var gZipStream = new GZipStream(memoryStream, CompressionMode.Compress, true))
+                {
+                    gZipStream.Write(buffer, 0, buffer.Length);
+                }
+                memoryStream.Position = 0;
+                var compressedData = new byte[memoryStream.Length];
+                memoryStream.Read(compressedData, 0, compressedData.Length);
+                var gZipBuffer = new byte[compressedData.Length + 4];
+                Buffer.BlockCopy(compressedData, 0, gZipBuffer, 4, compressedData.Length);
+                Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gZipBuffer, 0, 4);
+                return Convert.ToBase64String(gZipBuffer);
+            }
+        }
+
+        public string Unzip(string value, CompressionMethods method = CompressionMethods.gzip)
+        {
+            var gZipBuffer = Convert.FromBase64String(value);
+            using (var memoryStream = new MemoryStream())
+            {
+                var dataLength = BitConverter.ToInt32(gZipBuffer, 0);
+                memoryStream.Write(gZipBuffer, 4, gZipBuffer.Length - 4);
+                var buffer = new byte[dataLength];
+                memoryStream.Position = 0;
+                using (var gZipStream = new GZipStream(memoryStream, CompressionMode.Decompress))
+                {
+                    gZipStream.Read(buffer, 0, buffer.Length);
+                }
+                return Encoding.UTF8.GetString(buffer);
+            }
+        }
+    }
+}
