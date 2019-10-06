c34de886a74f89eb59c0fbc0e353c09d68af1828
Daren May <daren.may@gmail.com>
2019-10-05 21:05:22 -0600

    Created a FileActivation Sample

--- Samples/Template10.NuGet.FileActivationSample/App.xaml.cs
+++ Samples/Template10.NuGet.FileActivationSample/App.xaml.cs
@@ -1,100 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Template10.Navigation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Prism.Ioc;
+using System.Threading.Tasks;
+using Template10.Ioc;
+using Template10.Services;
+using Template10.NuGet.FileActivationSample.ViewModels;
+using Template10.NuGet.FileActivationSample.Views;
+using Windows.ApplicationModel.Activation;
+using System.Collections.Generic;
+using Windows.Storage;
+using System.Linq;
 
 namespace Template10.NuGet.FileActivationSample
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    sealed partial class App : Application
+    sealed partial class App : Template10.ApplicationBase
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
-        public App()
+        private INavigationService _nav;
+
+        public App() => InitializeComponent();
+
+        public override void RegisterTypes(IContainerRegistry container)
         {
-            this.InitializeComponent();
-            this.Suspending += OnSuspending;
+            container.RegisterView<MainPage, MainPageViewModel>();
+            container.RegisterView<MarkdownPage, MarkdownPageViewModel>();
+            container.Register<IDialogService, DialogService>();
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        public override void OnInitialized()
         {
-            Frame rootFrame = Window.Current.Content as Frame;
+            var frame = new Frame();
+            Window.Current.Content = new ShellPage { Frame = frame };
+            Window.Current.Activate();
+            _nav = NavigationFactory
+                .Create(frame, Guid.Empty.ToString())
+                .AttachGestures(Window.Current, Gesture.Back, Gesture.Forward, Gesture.Refresh);
+        }
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
+        public override async Task OnStartAsync(IStartArgs args)
+        {
+            switch (args.StartCause)
             {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
-
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
+                case StartCauses.File when (args.Arguments is FileActivatedEventArgs fileArgs):
+                    var navParams = new NavigationParameters(("file", fileArgs.Files.First()));
+                    await _nav.NavigateAsync(nameof(MarkdownPage), navParams);
+                    break;
+                default:
+                    await _nav.NavigateAsync(nameof(MainPage));
+                    break;
             }
-
-            if (e.PrelaunchActivated == false)
-            {
-                if (rootFrame.Content == null)
-                {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
-                }
-                // Ensure the current window is active
-                Window.Current.Activate();
-            }
-        }
-
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
-
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
-            deferral.Complete();
         }
     }
 }
---
+++ Samples/Template10.NuGet.FileActivationSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Template10.Mvvm;
+using Template10.Navigation;
+
+namespace Template10.NuGet.FileActivationSample.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            Title = "The application was launched normally.";
+        }
+    }
+}
